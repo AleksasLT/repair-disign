bb6ea2ea0eddd3164430cc8ffb61275e03d85ae2
AleksasLT <alex.yanishauskas@gmail.com>
2019-12-11 00:12:47 +0300

    feat: add gulp sass in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,28 @@
-const gulp = require('gulp');
+const {src, dest, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
 const csso = require('gulp-csso');
 const concat = require('gulp-concat');
+const sass = require('gulp-sass');
 
 // Static server
-gulp.task('browser-sync', function() {
+function bs() {
+  serveSass();
   browserSync.init({
       server: {
           baseDir: "./"
       }
   });
-  gulp.watch("./*.html").on('change', browserSync.reload);
-});
+  watch("./*.html").on('change', browserSync.reload);
+  watch("./sass/**/*.sass", serveSass);
+  watch("./js/*.js").on('change', browserSync.reload);
+};
 
-// Сreate minify task
-gulp.task('minify', function (){
-  gulp.src('./css/style.css')
-  .pipe(csso())
-  .pipe(concat('style.min.css'))
-  .pipe(gulp.dest('./css'));
-});
+// Compile sass into CSS & auto-inject into browsers
+function serveSass() {
+  return src("./sass/*.sass")
+      .pipe(sass())
+      .pipe(dest("./css"))
+      .pipe(browserSync.stream());
+};
+
+exports.serve = bs;
