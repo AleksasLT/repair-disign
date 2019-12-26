33707adcbfd2fa2a9f620d3e31baa78dd3e8bdeb
AleksasLT <alex.yanishauskas@gmail.com>
2019-12-26 20:10:53 +0300

    feat: add jquery.min.js libary

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-  const modal = document.querySelector('.modal');
-  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-  const closeBtn = document.querySelector('.modal__close');
-  const switchModal = () => {
-    modal.classList.toggle('modal--visible');
-  }
-  modalBtn.forEach(element => {
-    element.addEventListener('click', switchModal);
-  });
-  closeBtn.addEventListener('click', switchModal);
+$( document ).ready(function() { 
+let modal = $('.modal'),
+    modalBtn = $('[data-toggle=modal]'),
+    closeBtn = $('.modal__close');
+
+    modalBtn.on('click', function (){
+      modal.toggleClass('modal--visible')
+    });
+    closeBtn.on('click', function (){
+      modal.removeClass('modal--visible')
+    }); 
+  //console.log(modal);
 });
