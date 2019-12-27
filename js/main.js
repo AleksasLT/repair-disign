d6fd08feb88caf85bb8977a3969b7502ce3d4055
AleksasLT <alex.yanishauskas@gmail.com>
2019-12-27 11:30:38 +0300

    feat: add button scroll up

--- js/main.js
+++ js/main.js
@@ -1,13 +1,23 @@
 $( document ).ready(function() { 
 let modal = $('.modal'),
     modalBtn = $('[data-toggle=modal]'),
-    closeBtn = $('.modal__close');
+    closeBtn = $('.modal__close'),
+    upBtn = $('.button__up');
 
     modalBtn.on('click', function (){
       modal.toggleClass('modal--visible')
     });
     closeBtn.on('click', function (){
       modal.removeClass('modal--visible')
-    }); 
+    });
+    
+    $(window).scroll(function(event){
+      event.preventDefault();
+      upBtn.toggleClass('button__up--visible');
+      //console.log('Крути колесо');
+    });
+    upBtn.on('click', function (){
+      $('html, body').animate({scrollTop: 0}, 1500);
+    })
   //console.log(modal);
 });
