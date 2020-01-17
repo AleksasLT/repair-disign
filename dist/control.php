b175f604028442f6df78730385df23a19b29674e
AleksasLT <alex.yanishauskas@gmail.com>
2020-01-17 11:44:13 +0300

    feat: add gulp scrips for optimization size files

---
+++ dist/control.php
@@ -0,0 +1,51 @@
+<?php
+
+$userName = $_POST['userName'];
+$userPhone = $_POST['userPhone'];
+
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+
+
+// Load Composer's autoloader
+require 'php.mailer/Exception.php';
+require 'php.mailer/PHPMailer.php';
+require 'php.mailer/SMTP.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+$mail->CharSet = 'UTF-8';
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'ssl://smtp.mail.ru';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'test_tester_2001@mail.ru';                     // SMTP username
+    $mail->Password   = 'Dovile030291';                         // SMTP password
+    $mail->SMTPSecure = 'ssl';                                  // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+    $mail->Port       = 465;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('test_tester_2001@mail.ru', 'Заяка с сайта');
+    $mail->addAddress('398393@mail.ru');
+
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = "Новая заявка с сайта / Раздел Онлайн контроль";
+    $mail->Body    = "Имя пользователя: ${userName}, его телефон: ${userPhone}";
+
+
+    if ($mail->send()) {
+        echo "ok";
+    } else {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+    
+} 
+catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
-const {src, dest, watch} = require('gulp');
+const {src, dest, watch, series} = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const minify = require('gulp-minify');
+const htmlmin = require('gulp-htmlmin');
+const tinypng = require('gulp-tinypng-compress');
 
 // Static server
 function bs() {
@@ -20,8 +25,62 @@ function bs() {
 function serveSass() {
   return src("./sass/**/*.sass", "./sass/**/*.scss")
       .pipe(sass())
+      .pipe(autoprefixer({
+        cascade: false
+    }))
       .pipe(dest("./css"))
       .pipe(browserSync.stream());
 };
 
+function buildCSS(done) {
+  src("css/**/**.css")
+  .pipe(cleanCSS({compatibility: 'ie8'}))
+  .pipe(dest('dist/css/'));
+  done();
+};
+
+function buildJS(done) {
+  src(["js/**.js", "!js/**.min.js"])
+    .pipe(minify({ext:{
+      min:'.js'
+    }
+}))
+    .pipe(dest('dist/js/'));
+  src("js/**.min.js")
+    .pipe(dest('dist/js/'));
+  done();
+}
+
+function buildHTML(done) {
+  src("**.html")
+  .pipe(htmlmin({ collapseWhitespace: true }))
+  .pipe(dest("dist/"));
+  done();
+}
+
+function buildPHP(done) {
+  src("**.php")
+    .pipe(dest("dist/"));
+  src("php.mailer/**/**")
+    .pipe(dest("dist/php.mailer/"));
+  done();
+}
+
+function buildFonts(done) {
+  src("fonts/**/**")
+    .pipe(dest("dist/fonts/"));
+  done();
+}
+
+function imageMin(done) {
+  src("img/**/*.{jpg, png, jpeg}")
+    .pipe(tinypng({
+      key: '1HlLxfGsJ8C4NtvzlCZBsVgVBg4jkHmJ'}))
+    .pipe(dest("dist/img/"));
+  src("img/**/**")
+    .pipe(dest("dist/img/")) 
+  done();
+  }
+
 exports.serve = bs;
+exports.build = series(buildCSS, buildJS, buildHTML, buildPHP, buildFonts, imageMin);
